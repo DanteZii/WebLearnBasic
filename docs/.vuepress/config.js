1e829956a86ea386a14dda19e1336bfe63c21382
Dante <guge862055200@gmail.com>
2019-04-06 00:20:59 +0200

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Javascript', link: '/javascript/' },
+        ]
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,7 @@
+---
+home: true
+heroImage: /hero.png
+heroText: WebLearnBasic
+tagline: WebLearnBasic
+actionText: 快速上手 →
+actionLink: /zh/guide/
